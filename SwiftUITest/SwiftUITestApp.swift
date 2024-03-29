20de3b914d6eee71efa15b01567ae56983558f5e
Hovik Melikyan <hovik.melikyan@gmail.com>
2024-03-29 09:29:06 +0400

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Hovik Melikyan on 29.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
