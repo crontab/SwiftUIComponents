ff2918a11cf09f0374a5c990444cff9fcb34c782
Hovik Melikyan <hovik.melikyan@gmail.com>
2024-03-29 09:30:02 +0400

    Initial commit

--- SwiftUITest/SwiftUITestApp.swift
+++ SwiftUITest/SwiftUITestApp.swift
@@ -7,11 +7,12 @@
 
 import SwiftUI
 
+
 @main
 struct SwiftUITestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
