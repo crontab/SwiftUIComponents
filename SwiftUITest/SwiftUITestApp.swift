a72b32efef7f2b458fd5fc00e17269d60bc1963c
Hovik Melikyan <hovik.melikyan@gmail.com>
2024-07-01 23:57:41 +0400

    NavigationView weird animation experiments

--- SwiftUITest/SwiftUITestApp.swift
+++ SwiftUITest/SwiftUITestApp.swift
@@ -10,9 +10,55 @@ import SwiftUI
 
 @main
 struct SwiftUITestApp: App {
+	@State private var state: Bool = false
+
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+//			ContentView()
+			Group {
+				if state {
+					View1(state: $state, bgColor: .black.opacity(0.1))
+				}
+				else {
+					View1(state: $state, bgColor: .yellow.opacity(0.1))
+				}
+			}
+			.animation(.default, value: state)
 		}
 	}
+}
+
+
+// MARK: - Experiment
+
+struct View1: View {
+	@Binding var state: Bool
+	let bgColor: Color
+
+	var body: some View {
+		NavigationStack {
+			ZStack {
+				VStack {
+					Spacer()
+					Button{
+						state.toggle()
+					} label: {
+						Text("Flip")
+							.foregroundColor(.primary)
+							.frame(height: 64)
+							.frame(maxWidth: .infinity)
+							.background(.yellow)
+					}
+					.containerShape(Capsule())
+				}
+				.padding(24)
+			}
+			.background(bgColor)
+		}
+	}
+}
+
+
+#Preview {
+	View1(state: .constant(true), bgColor: .black.opacity(0.1))
 }
