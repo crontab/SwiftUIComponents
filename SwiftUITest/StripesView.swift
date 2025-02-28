e5d4f3d262cecf7acba06d886d8a3d9d5e9a7f15
Hovik Melikyan <hovik.melikyan@gmail.com>
2025-02-28 09:25:26 +0100

    Improved stripes

--- SwiftUITest/StripesView.swift
+++ SwiftUITest/StripesView.swift
@@ -7,37 +7,29 @@
 import SwiftUI
 
 
-// Stripes are based on https://github.com/eneko/Stripes
+// Based on https://github.com/eneko/Stripes
 
 struct Stripes: View {
+	@Environment(\.colorScheme) var colorScheme
 
-	struct StripesConfig {
-		var background: Color = .gray.opacity(0.05)
-		var foreground: Color = .gray.opacity(0.08)
-		var degrees: Double = 45
-		var barWidth: CGFloat = 8
-		var barSpacing: CGFloat = 10
-
-		static let `default` = StripesConfig()
-	}
-
-	var config: StripesConfig = .default
+	private let barWidth: CGFloat = 8
+	private let barSpacing: CGFloat = 10
 
 	var body: some View {
 		GeometryReader { geometry in
 			let longSide = max(geometry.size.width, geometry.size.height)
-			let itemWidth = config.barWidth + config.barSpacing
+			let itemWidth = barWidth + barSpacing
 			let items = Int(2 * longSide / itemWidth)
-			HStack(spacing: config.barSpacing) {
+			HStack(spacing: barSpacing) {
 				ForEach(0..<items, id: \.self) { index in
-					config.foreground
-						.frame(width: config.barWidth, height: 2 * longSide)
+					Rectangle()
+						.fill(.gray.opacity(colorScheme == .dark ? 0.15 : 0.05))
+						.frame(width: barWidth, height: 2 * longSide)
 				}
 			}
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
-			.rotationEffect(Angle(degrees: config.degrees), anchor: .center)
+			.rotationEffect(Angle(degrees: 45), anchor: .center)
 			.offset(x: -longSide / 2, y: -longSide / 2)
-			.background(config.background)
 		}
 		.clipped()
 	}
@@ -56,9 +48,8 @@ struct StripesView: View {
 			if let text {
 				Text(text.uppercased())
 					.font(.caption)
-					.foregroundColor(.gray)
+					.foregroundColor(.secondary)
 					.padding(EdgeInsets(top: 4, leading: 6, bottom: 3, trailing: 6))
-					.background(.white)
 			}
 		}
 	}
