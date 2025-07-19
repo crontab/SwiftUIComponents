67b1b71c18976b968026805afe1315bab5fccc83
Hovik Melikyan <hovik.melikyan@gmail.com>
2025-07-19 18:19:53 +0200

    Player progress (I)

---
+++ SwiftUIComponents/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIComponents
+//
+//  Created by Hovik Melikyan on 19.07.25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+	var body: some View {
+//		PlayerProgressView(total: 30, value: 10)
+	}
+}
+
+#Preview {
+	ContentView()
+}
--- SwiftUIComponents/SwiftUIComponentsApp.swift
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -12,7 +12,7 @@ struct SwiftUIComponentsApp: App {
 
 	var body: some Scene {
 		WindowGroup {
-
+			ContentView()
 		}
 	}
 }
